26450fec01cda95403bdd82566eb3cf7b044b122
sronning <sronning@umich.edu>
2019-10-11 15:31:43 -0400

    added video and fading

--- animate.js
+++ animate.js
@@ -1,3 +1,12 @@
+//----------------------------------------------------------------------------
+//VIDEO HANDLING BEGIN
+//----------------------------------------------------------------------------
+Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+    get: function(){
+        return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
+    }
+})
+
 const enlarge = (evt) => {
     if (evt.target.classList.contains('hoverEnl')) {
         if (evt.target.classList.contains('deEnlarge')) {
@@ -32,10 +41,64 @@ const deEnlarge = (evt) => {
     }
 }
 
+const vidOpen = (evt) => {
+    let c = evt.target.children;
+    let vid;
+    let extra;
+    for (let i = 0; i < c.length; i++) {
+        if (c[i].tagName == 'VIDEO') {
+            vid = c[i];
+        }
+        if (c[i].classList.contains('extra')) {
+            extra = c[i];
+        }
+    }
+    vid.play();
+    extra.classList.remove('fadeIn');
+    extra.classList.remove('fadeOut');
+    extra.classList.remove('animated');
+    extra.classList.add('fadeIn');
+    extra.classList.add('animated');
+    vid.classList.remove('fadeIn');
+    vid.classList.remove('fadeOut');
+    vid.classList.remove('animated');
+    vid.classList.add('fadeIn');
+    vid.classList.add('animated');
+}
+
+const vidClose = (evt) => {
+    let c = evt.target.children;
+    let vid;
+    let extra;
+    for (let i = 0; i < c.length; i++) {
+        if (c[i].tagName == 'VIDEO') {
+            vid = c[i];
+        }
+        if (c[i].classList.contains('extra')) {
+            extra = c[i];
+        }
+    }
+    extra.classList.remove('fadeIn');
+    extra.classList.remove('fadeOut');
+    extra.classList.remove('animated');
+    extra.classList.add('fadeOut');
+    extra.classList.add('animated');
+    vid.classList.remove('fadeIn');
+    vid.classList.remove('fadeOut');
+    vid.classList.remove('animated');
+    vid.classList.add('fadeOut');
+    vid.classList.add('animated');
+    vid.pause();
+}
+
 let toBeEnlrg = document.getElementsByClassName('hoverEnl');
-console.log(toBeEnlrg);
 
 for (let i = 0; i < toBeEnlrg.length; i++) {
     toBeEnlrg[i].addEventListener("mouseover", enlarge);
+    toBeEnlrg[i].addEventListener("mouseover", vidOpen);
     toBeEnlrg[i].addEventListener("mouseout", deEnlarge);
+    toBeEnlrg[i].addEventListener("mouseout", vidClose);
 }
+//----------------------------------------------------------------------------
+//VIDEO HANDLING END
+//----------------------------------------------------------------------------
